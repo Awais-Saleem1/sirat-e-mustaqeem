5d1ac62a4b8a5ffab5e0a894bbffb497a61997c6
Awais Saleem <awaissaleem2good@gmail.com>
2024-04-02 22:28:50 +0500

    navigation issue fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
 const container = document.getElementById("root");
@@ -9,7 +9,7 @@ const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
-  <HashRouter>
+  <BrowserRouter basename="/sirat-e-mustaqeem">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
