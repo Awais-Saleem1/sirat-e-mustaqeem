56f66b8076f2dd9c513abe063b39acc83a721ffa
Awais Saleem <awaissaleem2good@gmail.com>
2024-04-02 22:14:30 +0500

    navigation issue fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-/**
-=========================================================
-* E Learning React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-kit-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { useEffect } from "react";
-
-// react-router components
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
-// @mui material components
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
